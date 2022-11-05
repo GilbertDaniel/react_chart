ebf2e0fbd5d9045bb45dce2cb261c8e26fc98567
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-11-05 12:52:10 +0630

    Integrate Bar Chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart';
 
 function App() {
   return (
     <div className="App">
       <h1>Data Visualization with React and Chart.js</h1>
+      <BarChart/>
     </div>
   );
 }
